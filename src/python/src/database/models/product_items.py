fbd9bc8836be7f107be37a94637217201e888d0f
Redr1ghthand <4aikaman@gmail.com>
2024-03-19 17:46:21 +0200

    feature(database) category_items.py, product_items.py
    add column current_price into db models

--- src/python/src/database/models/product_items.py
+++ src/python/src/database/models/product_items.py
@@ -11,6 +11,7 @@ class ProductItems(Base):
     external_id = Column('external_id', String(255), unique=True, nullable=False)
     title = Column('title', String(255), unique=False, nullable=False)
     normal_price = Column('normal_price', DECIMAL(20, 2))
+    current_price = Column('current_price', DECIMAL(20, 2))
     stock = Column('stock', MEDIUMINT(unsigned=True), server_default=text("0"))
     brand = Column('brand', String(255))
     image_url = Column('image_url', String(255))
