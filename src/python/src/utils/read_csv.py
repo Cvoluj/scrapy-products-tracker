9b33e12377ee86336926c3692ff41895dbed87b2
cvoluj <berezanskiydaniiil@gmail.com>
2024-03-27 21:16:28 +0200

    feat(sessions): csvdatabase work with sessions
    callback for create_sessions, get_session before inserting product/category
    product_targets, category_targets on inserting add current session id

--- src/python/src/utils/read_csv.py
+++ src/python/src/utils/read_csv.py
@@ -17,30 +17,33 @@ class CSVDatabase:
         self.csv_file = csv_file
 
     def insert_from_csv(self):
+        d = self.create_session()
+        d.addCallback(lambda _: self.get_session())
+        d.addCallback(self.process_csv_with_session)
+        
+    def process_csv_with_session(self, session):
         with open(self.csv_file, mode='r') as file:
             reader = csv.reader(file)
             next(reader)
-            logging.warning('called')
-            self.create_session()
-            logging.warning('ended')
             for row in reader:
                 domain = self.parse_domain(row[0])
-                self.process_row(row[0], domain)
+                self.process_row(row[0], domain, session)
 
-    def process_row(self, row, domain):
+    def process_row(self, row, domain, session):
         try:
             values = {
                 "url": row,
                 "domain": domain,
-                "session": None
+                "session": session[0]['id']
             }
-            logging.warning(self.get_session())
+            
             if type(self.model) is ProductTargets:
                 values['external_id'] = row
                 
             stmt: Insert = insert(self.model)
             stmt = stmt.on_duplicate_key_update({
-                'status': TaskStatusCodes.NOT_PROCESSED
+                'status': TaskStatusCodes.NOT_PROCESSED,
+                "session": session[0]['id']
             }).values(**values)
 
             self.conn.runQuery(*compile_expression(stmt))
@@ -50,7 +53,7 @@ class CSVDatabase:
     def create_session(self):
         try:
             stmt: Insert = insert(Sessions).values(csv_file=self.csv_file)
-            self.conn.runQuery(*compile_expression(stmt))
+            return self.conn.runQuery(*compile_expression(stmt))
         except Exception as e:
             logging.error("Error inserting item: %s", e)
     
@@ -67,4 +70,4 @@ if __name__ == '__main__':
     csv_file = 'csv_file.csv'
     csvdatabase = CSVDatabase(csv_file)
     csvdatabase.insert_from_csv()
-    reactor.run
+    reactor.run()
