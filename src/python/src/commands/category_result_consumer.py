593ee13cfe49b0a3e1464530785d3fdc71723fb5
Rostyslav Okhochyi <ohochyi2002@gmail.com>
2024-04-08 13:46:40 +0300

    docs(consumers): add docstring for consumer commands

--- src/python/src/commands/category_result_consumer.py
+++ src/python/src/commands/category_result_consumer.py
@@ -1,6 +1,5 @@
 from sqlalchemy.dialects.mysql import insert
 from furl import furl
-
 from rmq.commands import Consumer
 from rmq.utils import TaskStatusCodes
 from database.models import ProductTargets
@@ -8,14 +7,35 @@ from database.models import ProductTargets
 
 class CategoryResultConsumer(Consumer):
     """
+    A consumer class for processing messages related to category results.
+
     Example of calling this command:
     scrapy category_result_consumer -m worker
     """
     def __init__(self):
+        """
+        Initialize the CategoryResultConsumer.
+
+        This sets the queue_name attribute using project_settings.
+
+        Returns:
+            None
+        """
+
         super().__init__()
         self.queue_name = self.project_settings.get("RMQ_CATEGORY_RESULT_QUEUE")
 
     def build_message_store_stmt(self, message_body):
+        """
+        Build an SQLAlchemy insert statement for storing message data.
+
+        Args:
+            message_body (dict): The body of the message containing 'url', 'position', 'session', and 'category'.
+
+        Returns:
+            sqlalchemy.sql.dml.Insert: The SQLAlchemy insert statement.
+        """
+
         url = message_body.get('url')
         product_targets_stmt = insert(ProductTargets).values(
             url=url,
